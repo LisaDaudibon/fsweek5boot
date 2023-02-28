ce386f0973622d88ffcfac92f5afd2d09c2601a9
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-02-28 19:23:23 +0100

    Atoms & molecules

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,19 @@
+class StaticPageController < ApplicationController
+  def index
+  end
+
+  def navbar
+  end
+
+  def footer
+  end
+
+  def authentication_form
+  end
+  
+  def banner
+  end
+
+  def card
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_page#navbar"
+  get 'static_page/index'
+  get 'static_page/navbar'
 end
