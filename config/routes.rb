aa84e0c73f69a7fac91e0819acb1da94af03c101
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-03-01 07:05:40 +0100

    New day, new me (modifs index, navbar, footer)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "static_page#navbar"
+  root "static_page#index"
   get 'static_page/index'
   get 'static_page/navbar'
+  get 'static_page/footer'
+  get 'static_page/authentication_form'
+  get 'static_page/card'
+  get 'static_page/banner'
 end
